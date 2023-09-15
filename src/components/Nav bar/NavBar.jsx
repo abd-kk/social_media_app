aeb42c0938be566e6c0e1bedd80ea7d9b91f2911
Abdallah <abdallahkorhani1@gmail.com>
2023-09-15 09:19:51 +0300

    first commit

--- src/components/Nav bar/NavBar.jsx
+++ src/components/Nav bar/NavBar.jsx
@@ -6,16 +6,22 @@ import {
   faBell,
   faMessage,
   faBars,
+  faArrowRightFromBracket,
+  faMoon,
+  faGear,
+  faArrowRight,
+  faArrowLeft,
 } from "@fortawesome/free-solid-svg-icons";
 import { Auth } from "../AuthenticationContext";
-import { useContext, useState, createRef } from "react";
+import { useContext, useState, createRef, useEffect } from "react";
 import { DocSearch } from "../Document Search/DocSearch";
 import { createPortal } from "react-dom";
 import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
   const [showDocSearch, setShowDocSearch] = useState(false);
-  const { user, getUserInfo } = useContext(Auth);
+  const [showDropDownList, setshowDropDownList] = useState(false);
+  const { user, getUserInfo, logout } = useContext(Auth);
 
   const userId = user.userId;
 
@@ -23,8 +29,28 @@ export const NavBar = () => {
 
   const location = useLocation();
   const profileCircle = createRef();
+  const dropDownList = createRef();
+  const mainList = createRef();
+  const displayList = createRef();
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (e.target.classList.contains("profile-pic")) return;
+      if (!showDropDownList) return;
+      if (!dropDownList.current.contains(e.target)) {
+        console.log("Mo wai moo");
+        setshowDropDownList(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropDownList, showDropDownList]);
 
   const showDocumentSearch = () => {
     setShowDocSearch(true);
@@ -37,6 +63,24 @@ export const NavBar = () => {
   const handleProfileClick = () => {
     if (location.pathname === "/profile") return;
     navigate("/profile", { state: { userId: userId } });
+  };
+
+  const toggleDropDownList = (e) => {
+    if (dropDownList.current !== null)
+      if (dropDownList.current.contains(e.target)) return;
+    setshowDropDownList(!showDropDownList);
+  };
+
+  const showDisplayList = () => {
+    if (mainList.current !== null) mainList.current.classList.remove("active");
+    if (displayList.current !== null)
+      displayList.current.classList.add("active");
+  };
+
+  const hideDisplayList = () => {
+    if (mainList.current !== null) mainList.current.classList.add("active");
+    if (displayList.current !== null)
+      displayList.current.classList.remove("active");
   };
 
   return (
@@ -75,10 +119,83 @@ export const NavBar = () => {
         <div className="profile">
           <div
             ref={profileCircle}
-            onClick={handleProfileClick}
             className="profile-circle"
+            onClick={toggleDropDownList}
           >
-            <img src={userInfo.profile} alt="" />
+            <img className="profile-pic" src={userInfo.profile} />
+            {showDropDownList && (
+              <div className="drop-down-list" ref={dropDownList}>
+                <ul ref={mainList} className="main-list active">
+                  <div className="profile-info" onClick={handleProfileClick}>
+                    <div
+                      className="profile-circle"
+                      style={{ marginRight: "10px" }}
+                    >
+                      <img src={userInfo.profile} />
+                    </div>
+                    <div className="user-name">
+                      {userInfo.firstName} {userInfo.lastName}
+                    </div>
+                  </div>
+
+                  <li>
+                    <div>
+                      <FontAwesomeIcon className="icon" icon={faGear} />
+                      <span>Settings</span>
+                    </div>
+
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </li>
+                  <li onClick={showDisplayList}>
+                    <div>
+                      <FontAwesomeIcon className="icon" icon={faMoon} />
+                      <span>Display</span>
+                    </div>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </li>
+                  <li onClick={logout}>
+                    <div>
+                      <FontAwesomeIcon
+                        className="icon"
+                        icon={faArrowRightFromBracket}
+                      />
+                      <span>Log Out</span>
+                    </div>
+                  </li>
+                </ul>
+                <ul ref={displayList} className="display-list">
+                  <div className="list-header">
+                    <FontAwesomeIcon
+                      className="backIcon"
+                      onClick={hideDisplayList}
+                      icon={faArrowLeft}
+                    />
+                    <div className="list-title">Display</div>
+                  </div>
+                  <div className="list-content">
+                    <div className="display-option">
+                      <FontAwesomeIcon icon={faMoon} />
+                      <div className="display-option-content">
+                        <div>Dark Mode</div>
+                        <div>Make your eyes comfortable</div>
+                      </div>
+                      <form onSubmit={(e) => e.preventDefault()}>
+                        <div>
+                          <label htmlFor="">Off</label>
+
+                          <input type="radio" name="darkMode" />
+                        </div>
+                        <div>
+                          <label htmlFor="">Off</label>
+
+                          <input type="radio" name="darkmode" />
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
